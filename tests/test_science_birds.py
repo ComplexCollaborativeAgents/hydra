74c7cfbbb9b292bfe10cb66f71be44f51be99349
Wiktor Piotrowski <wiktorpi@wplaptop.parc.xerox.com>
2020-05-12 11:48:17 -0700

    parameter update

--- tests/test_science_birds.py
+++ tests/test_science_birds.py
@@ -21,12 +21,12 @@ from agent.hydra_agent import HydraAgent
 def launch_science_birds():
     print("starting")
     #remove config files
-    # cmd = 'cp {}/data/science_birds/level-14.xml {}/00001.xml'.format(str(settings.ROOT_PATH), str(settings.SCIENCE_BIRDS_LEVELS_DIR))
-    # subprocess.run(cmd, shell=True)
-    # cmd = 'cp {}/data/science_birds/level-15.xml {}/00002.xml'.format(str(settings.ROOT_PATH), str(settings.SCIENCE_BIRDS_LEVELS_DIR))
-    # subprocess.run(cmd, shell=True)
-    # cmd = 'cp {}/data/science_birds/level-16.xml {}/00003.xml'.format(str(settings.ROOT_PATH), str(settings.SCIENCE_BIRDS_LEVELS_DIR))
-    # subprocess.run(cmd, shell=True)
+    cmd = 'cp {}/data/science_birds/level-14.xml {}/00001.xml'.format(str(settings.ROOT_PATH), str(settings.SCIENCE_BIRDS_LEVELS_DIR))
+    subprocess.run(cmd, shell=True)
+    cmd = 'cp {}/data/science_birds/level-15.xml {}/00002.xml'.format(str(settings.ROOT_PATH), str(settings.SCIENCE_BIRDS_LEVELS_DIR))
+    subprocess.run(cmd, shell=True)
+    cmd = 'cp {}/data/science_birds/level-16.xml {}/00003.xml'.format(str(settings.ROOT_PATH), str(settings.SCIENCE_BIRDS_LEVELS_DIR))
+    subprocess.run(cmd, shell=True)
     env = sb.ScienceBirds(None,launch=True,config='test_config.xml')
     yield env
     print("teardown tests")
@@ -39,7 +39,7 @@ def test_science_birds_agent(launch_science_birds):
     hydra = HydraAgent(env)
     hydra.main_loop() # enough actions to play the first two levels
     scores = env.get_all_scores()
-    # assert len([x for x in scores if x > 0]) == 3 # solved two problems
+    assert len([x for x in scores if x > 0]) == 3 # solved two problems
 
 
 @pytest.mark.skipif(True, reason="headless does not work in docker")
