972efe356ef339667d759d30fafcacf3eaea79b6
klenk <klenk@parc.com>
2020-10-27 15:33:10 -0700

    changed defaults

--- utils/run_hydra_sb_planning_tests.py
+++ utils/run_hydra_sb_planning_tests.py
@@ -3,7 +3,7 @@ import worlds.science_birds as sb
 from agent.hydra_agent import HydraAgent
 import sys
 
-def main(config='all_level_0_novelties.xml',simplification_code=2):
+def main(config='30_level_0_novelties.xml',simplification_code=2):
     settings.DEBUG=False
     settings.SB_DEV_MODE=False
     settings.SB_PLANNER_SIMPLIFICATION_SEQUENCE = [simplification_code]
