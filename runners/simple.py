c3ed71b02813e2dee2df5e755b85754c3716fa4f
Alexandre Perez <aperez@parc.com>
2020-11-05 15:14:34 -0800

    Update docker img entrypoint.

--- runners/simple.py
+++ runners/simple.py
@@ -1,5 +1,5 @@
 import worlds.science_birds as sb
-from agent.hydra_agent import HydraAgent
+from agent.repairing_hydra_agent import RepairingHydraSBAgent
 import argparse
 from utils.host import Host
 
@@ -7,15 +7,15 @@ from utils.host import Host
 def parse():
     parser = argparse.ArgumentParser(description='HYDRA agent.')
     parser.add_argument('--server', type=Host.type, help='server host (format: hostname:port)')
-    parser.add_argument('--observer', type=Host.type, help='observer host (format: hostname:port)')
+    # parser.add_argument('--observer', type=Host.type, help='observer host (format: hostname:port)')
     arguments = parser.parse_args()
     return arguments
 
 
 def main():
     arguments = parse()
-    env = sb.ScienceBirds(None, launch=False, server_host=arguments.server, observer_host=arguments.observer)
-    hydra = HydraAgent(env)
+    env = sb.ScienceBirds(None, launch=False, host=arguments.server)
+    hydra = RepairingHydraSBAgent(env)
     hydra.main_loop()
 
 
