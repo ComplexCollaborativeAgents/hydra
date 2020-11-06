a9520d339f31e3032f2b95ac71dcb4a0952aa465
Alexandre Perez <aperez@parc.com>
2020-11-05 18:09:42 -0800

    Remove dev mode flag from science birds launch script.

--- bin/run_sb_standalone.sh
+++ bin/run_sb_standalone.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 CONFIG='../../data/science_birds/config/test_reinit.xml'
-FLAGS='--headless --dev'
+FLAGS='--headless'
 
 cd linux && java -jar game_playing_interface.jar --config-path $CONFIG $FLAGS
