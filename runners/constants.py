aec02d72b82ee9e660f3168fde8d89684c8daded
Shiwali Mohan <shiwali.mohan@parc.com>
2021-05-14 12:46:46 -0700

    updated constants

--- runners/constants.py
+++ runners/constants.py
@@ -1,3 +1,20 @@
 NON_NOVELTY_PERFORMANCE = 'non-novelty-performance'
 UNKNOWN_NOVELTY = 'unknown-novelty'
 KNOWN_NOVELTY = 'known-novelty'
+
+GRAVITY = 'gravity'
+MASSCART = 'masscart'
+MASSPOLE = 'masspole'
+LENGTH = 'length'
+FORCE_MAG = 'force_mag'
+
+MAX_SCORE = 200
+
+
+ENV_PARAMS_NOMINAL = {
+    GRAVITY: 9.8,
+    MASSCART: 1,
+    MASSPOLE: 0.1,
+    LENGTH: 0.5,
+    FORCE_MAG: 10.0
+}
