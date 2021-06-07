667c6602fccd1e19d0fbeddd16de0f02ebaa71fd
rstern <rstern@parc.com>
2021-06-07 11:49:42 -0700

    Added a test in test_sb_visualizer.py that runs the SB framework, extracts the observation of the last shot, and outputs the animations for the expected and observed plots.
    Added animate_expected() and animate_observed() to trace_visualizer.py

--- tests/test_sb_visualizer.py
+++ tests/test_sb_visualizer.py
@@ -1,23 +1,14 @@
-import matplotlib.pyplot as plt
-import pytest
-import time
-
-from agent.consistency.fast_pddl_simulator import *
-from agent.repair.sb_repair import BirdLocationConsistencyEstimator, ScienceBirdsConsistencyEstimator
+from agent.consistency.trace_visualizer import animate_expected, animate_observed
 from agent.hydra_agent import *
-from agent.planning.model_manipulator import ManipulateInitNumericFluent
 from agent.planning.planner import *
 from matplotlib import pyplot as plt
 from agent.perception.perception import *
-import tests.test_utils as test_utils
+from agent.repairing_hydra_agent import RepairingHydraSBAgent
 from tests.test_utils import create_logger
-from state_prediction.anomaly_detector_fc_multichannel import FocusedSBAnomalyDetector
 from os import listdir
 from agent.consistency import trace_visualizer as trace_visualizer
 logger = create_logger("test_sb_visualizer")
-import time
 PRECISION = 1
-import numpy as np
 
 # Constants for ScienceBirds
 SB_NON_NOVEL_OBS_DIR = path.join(settings.ROOT_PATH, 'data', 'science_birds', 'consistency', 'non_novel')
@@ -30,6 +21,33 @@ SB_NOVEL_TESTS = listdir(SB_NOVEL_OBS_DIR)
 logger = create_logger("test_sb_visualizer")
 
 
+
+def test_animate_single_shot():
+    config_file = 'test_repair_wood_health.xml'
+    expected_gif_file = "expected_trace.gif"
+    observed_gif_file = "observed_trace.gif"
+
+
+    # Setup
+    logger.info("Starting ScienceBirds")
+    try:
+        env = sb.ScienceBirds(None,launch=True,config=config_file)
+        hydra = RepairingHydraSBAgent(env)
+        meta_model = hydra.meta_model
+
+        hydra.run_next_action()
+        observation = hydra.find_last_obs()
+        animate_observed(path.join(TEST_DATA_DIR, observed_gif_file),
+                         observation, meta_model=meta_model)
+        animate_expected(path.join(TEST_DATA_DIR, expected_gif_file),
+                         observation, meta_model=meta_model)
+    finally:
+        # Teardown
+        if env is not None:
+            env.kill()
+            logger.info("Ending ScienceBirds")
+
+
 def test_animate_observed_and_expected():
     ''' test the animation functionality of the trace visualizer '''
     meta_model = ScienceBirdsMetaModel()
@@ -40,18 +58,25 @@ def test_animate_observed_and_expected():
         sb_ob = pickle.load(open(path.join(SB_NON_NOVEL_OBS_DIR, obs_file), "rb"))
 
         logger.info("Creating animation for the trace of obs {}".format(i))
-        obs_state_sequence = sb_ob.get_pddl_states_in_trace(meta_model)
-        fig = plt.figure()
-        ax = fig.add_subplot(1,1,1)
-        trace_animation = trace_visualizer.animate_trace(fig, ax, obs_state_sequence)
-        trace_animation.save(path.join(TEST_DATA_DIR,"animation-observed-{}.gif".format(i)))
+        animate_observed(path.join(TEST_DATA_DIR, "animation-observed-{}.gif".format(i)),
+                         sb_ob, meta_model=meta_model)
 
         logger.info("Creating animation for the expected trace of obs {}".format(i))
-        simulator = CachingPddlPlusSimulator()
-        sim_trace = simulator.simulate_observed_action(sb_ob.state, sb_ob.action, meta_model)
-        sim_state_sequence = [timed_state[0] for timed_state in sim_trace]
-        fig = plt.figure()
-        ax = fig.add_subplot(1,1,1)
-        trace_animation = trace_visualizer.animate_trace(fig, ax, sim_state_sequence)
-        trace_animation.save(path.join(TEST_DATA_DIR,"animation-expected-{}.gif".format(i)))
+        animate_expected(path.join(TEST_DATA_DIR, "animation-expected-{}.gif".format(i)),
+                         sb_ob, meta_model=meta_model)
+
+
+def test_animate_observed_and_expected():
+    ''' test the animation functionality of the trace visualizer '''
+    meta_model = ScienceBirdsMetaModel()
+    samples = 10
+
+    for i in range(samples):
+        obs_file = SB_NON_NOVEL_TESTS[i]
+        sb_ob = pickle.load(open(path.join(SB_NON_NOVEL_OBS_DIR, obs_file), "rb"))
+        logger.info("Creating animation for the trace of obs {}".format(i))
+        trace_visualizer.animate_observed(path.join(TEST_DATA_DIR,"animation-observed-{}.gif".format(i)), sb_ob, meta_model=meta_model)
+
+        logger.info("Creating animation for the expected trace of obs {}".format(i))
+        trace_visualizer.animate_expected(path.join(TEST_DATA_DIR,"animation-expected-{}.gif".format(i)), sb_ob, meta_model=meta_model)
 
