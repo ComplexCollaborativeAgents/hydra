fb66fe8c15d25faf2fe8f7dca1528f515b5efe6f
Shiwali Mohan <shiwali.mohan@parc.com>
2021-08-18 10:39:30 -0700

    updated params

--- runners/novelty_experiment_runner_sb.py
+++ runners/novelty_experiment_runner_sb.py
@@ -477,7 +477,7 @@ class NoveltyExperimentRunnerSB:
 
 
 if __name__ == '__main__':
-    experiment_runner = NoveltyExperimentRunnerSB(AgentType.Baseline, export_trials=False)
+    experiment_runner = NoveltyExperimentRunnerSB(AgentType.Baseline, export_trials=True)
 
     experiment_runner.run_experiment()
     # experiment_runner.run_experiment(configs=[SB_CONFIG_PATH / "trial_config_1_6.xml"])
