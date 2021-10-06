89e13fc496bbb47b683640f444c629a1d23a6419
jacob-le <jacob.phong.le@gmail.com>
2021-10-06 09:32:08 -1000

    Add before/after screenshot option for science birds world (only works for headed mode)

--- utils/generate_observations.py
+++ utils/generate_observations.py
@@ -3,6 +3,7 @@ import worlds.science_birds as sb
 from agent.sb_hydra_agent import SBHydraAgent
 import os.path as path
 import shutil
+from zipfile import ZipFile
 
 
 def main(config='all_level_0_novelties.xml'):
@@ -37,16 +38,24 @@ def eval_m18_data():
         shutil.rmtree(path.join(settings.ROOT_PATH,"agent","consistency","trace"))
 
 if __name__ == '__main__':
-    path_prefix = "M18"
-    config_files = ["100_level_3_type_7_novelties.xml"]
+    path_prefix = "Phase2"
+    config_files = ["100_level_0_type_2_novelties_r2.xml"] # ["200_level_1_type_9_novelties.xml", "200_level_1_type_10_novelties.xml"]
 
     for config_file in config_files:
         settings.SB_DEFAULT_SHOT = ''
-        main(config=path.join(path_prefix, config_file))
+        # main(config=path.join(path_prefix, config_file))
+
+        # copy_path_baseline = path.join(settings.ROOT_PATH, "data", "science_birds", config_file[:-4], "baseline")
+
+        # shutil.copytree(path.join(settings.ROOT_PATH,"agent","consistency","trace"),
+        #                 copy_path_baseline)
+        # shutil.rmtree(path.join(settings.ROOT_PATH,"agent","consistency","trace"))
+
         settings.SB_DEFAULT_SHOT = 'RANDOM'
         main(config=path.join(path_prefix, config_file))
 
-        shutil.copytree(path.join(settings.ROOT_PATH,"agent","consistency","trace"),
-                        path.join(settings.ROOT_PATH, "data", "science_birds", config_file[:-4]))
+        random_path = path.join(settings.ROOT_PATH, "data", "science_birds", config_file[:-4])
 
+        shutil.copytree(path.join(settings.ROOT_PATH,"agent","consistency","trace"),
+                        random_path)
         shutil.rmtree(path.join(settings.ROOT_PATH,"agent","consistency","trace"))
