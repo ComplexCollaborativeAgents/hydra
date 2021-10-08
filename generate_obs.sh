acb3a6e7d34bb53d6cdb00875ef657dd75dda5c4
Jacob Le <jale@parc.com>
2021-10-08 13:08:49 -0700

    update generate obs utils

--- generate_obs.sh
+++ generate_obs.sh
@@ -1,64 +1,64 @@
-for i in {1..4}
+for i in {1..2}
 do
     echo "welcome $i random"
-    python utils/generate_observations.py --random --config-file all_level_0_novelties.xml
-    killall 9001.x86_64
+    python utils/generate_observations.py -c 50_level_1_type_9_novelties_r1.xml
+    kill -9 $(pgrep -f 9001.x86_64)
     sleep 1m
 done
 
-for i in {1..4}
+for i in {1..2}
 do
     echo "welcome $i"
-    python utils/generate_observations.py --non-random --config-file all_level_0_novelties.xml
-    killall 9001.x86_64
+    python utils/generate_observations.py -c 50_level_1_type_9_novelties_r2.xml
+    kill -9 $(pgrep -f 9001.x86_64)
     sleep 1m
 done
 
 
-for i in {1..4}
+for i in {1..2}
 do
     echo "welcome $i random"
-    python utils/generate_observations.py --random --config-file 100_level_1_novelties.xml
-    killall 9001.x86_64
+    python utils/generate_observations.py -c 50_level_1_type_9_novelties_r3.xml
+    kill -9 $(pgrep -f 9001.x86_64)
     sleep 1m
 done
 
-for i in {1..4}
+for i in {1..2}
 do
     echo "welcome $i"
-    python utils/generate_observations.py --non-random --config-file 100_level_1_novelties.xml
-    killall 9001.x86_64
+    python utils/generate_observations.py -c 50_level_1_type_9_novelties_r4.xml
+    kill -9 $(pgrep -f 9001.x86_64)
     sleep 1m
 done
 
-for i in {1..4}
+for i in {1..2}
 do
     echo "welcome $i random"
-    python utils/generate_observations.py --random --config-file 100_level_2_novelties.xml
-    killall 9001.x86_64
+    python utils/generate_observations.py -c 50_level_1_type_10_novelties_r1.xml
+    kill -9 $(pgrep -f 9001.x86_64)
     sleep 1m
 done
 
-for i in {1..4}
+for i in {1..2}
 do
     echo "welcome $i"
-    python utils/generate_observations.py --non-random --config-file 100_level_2_novelties.xml
-    killall 9001.x86_64
+    python utils/generate_observations.py -c 50_level_1_type_10_novelties_r2.xml
+    kill -9 $(pgrep -f 9001.x86_64)
     sleep 1m
 done
 
-for i in {1..4}
+for i in {1..2}
 do
     echo "welcome $i random"
-    python utils/generate_observations.py --random --config-file 100_level_3_novelties.xml
-    killall 9001.x86_64
+    python utils/generate_observations.py -c 50_level_1_type_10_novelties_r3.xml
+    kill -9 $(pgrep -f 9001.x86_64)
     sleep 1m
 done
 
-for i in {1..4}
+for i in {1..2}
 do
     echo "welcome $i"
-    python utils/generate_observations.py --non-random --config-file 100_level_3_novelties.xml
-    killall 9001.x86_64
+    python utils/generate_observations.py -c 50_level_1_type_10_novelties_r4.xml
+    kill -9 $(pgrep -f 9001.x86_64)
     sleep 1m
 done
--- utils/generate_observations.py
+++ utils/generate_observations.py
@@ -3,7 +3,11 @@ import worlds.science_birds as sb
 from agent.sb_hydra_agent import SBHydraAgent
 import os.path as path
 import shutil
+<<<<<<< Updated upstream
+=======
+import argparse
 from zipfile import ZipFile
+>>>>>>> Stashed changes
 
 
 def main(config='all_level_0_novelties.xml'):
@@ -38,24 +42,38 @@ def eval_m18_data():
         shutil.rmtree(path.join(settings.ROOT_PATH,"agent","consistency","trace"))
 
 if __name__ == '__main__':
-    path_prefix = "Phase2"
-    config_files = ["100_level_0_type_2_novelties_r2.xml"] # ["200_level_1_type_9_novelties.xml", "200_level_1_type_10_novelties.xml"]
+    path_prefix = "M18"
+    config_files = ["100_level_3_type_7_novelties.xml"]
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', type=str, help='path prefix after hydra/data/science_birds/config', default=None, dest='path_prefix')
+    parser.add_argument('-c', type=str, nargs='+', help='list of config files to run', default=None, dest='config_files')
+
+    args = parser.parse_args()
+
+    if args.path_prefix is not None:
+        print("Using args path_prefix: {}".format(args.path_prefix))
+        path_prefix = args.path_prefix
+    if args.config_files is not None:
+        print("Using args config_files: {}".format(args.config_files))
+        config_files = args.config_files
 
     for config_file in config_files:
         settings.SB_DEFAULT_SHOT = ''
-        # main(config=path.join(path_prefix, config_file))
+        main(config=path.join(path_prefix, config_file))
 
-        # copy_path_baseline = path.join(settings.ROOT_PATH, "data", "science_birds", config_file[:-4], "baseline")
+        copy_path_baseline = path.join(settings.ROOT_PATH, "data", "science_birds", config_file[:-4], "baseline")
 
-        # shutil.copytree(path.join(settings.ROOT_PATH,"agent","consistency","trace"),
-        #                 copy_path_baseline)
-        # shutil.rmtree(path.join(settings.ROOT_PATH,"agent","consistency","trace"))
+        shutil.copytree(path.join(settings.ROOT_PATH,"agent","consistency","trace"),
+                        copy_path_baseline)
+        shutil.rmtree(path.join(settings.ROOT_PATH,"agent","consistency","trace"))
 
         settings.SB_DEFAULT_SHOT = 'RANDOM'
         main(config=path.join(path_prefix, config_file))
 
-        random_path = path.join(settings.ROOT_PATH, "data", "science_birds", config_file[:-4])
+        copy_path_random = path.join(settings.ROOT_PATH, "data", "science_birds", config_file[:-4], "random")
 
         shutil.copytree(path.join(settings.ROOT_PATH,"agent","consistency","trace"),
-                        random_path)
+                        copy_path_random)
+
         shutil.rmtree(path.join(settings.ROOT_PATH,"agent","consistency","trace"))
