4f96a1f72845b2c56a794efce370292d80136c26
Meghna <meghnag@seas.upenn.edu>
2021-11-16 13:05:22 -0500

    sparsity based novelty detector

---
+++ agent/perception/novelty_detection/novelty_detector.py
@@ -0,0 +1,120 @@
+import numpy as np
+import torch
+from shapely.geometry import Polygon
+import cv2
+from PIL import Image
+
+from agent.perception.novelty_detection.model_cosine import ModelCosine
+import torchvision.transforms as transforms
+
+import pickle
+import pdb
+
+class ObsToState():
+
+    def Unpack_ScienceBirdsObservation(self,obs):
+        sb_state = obs.state  # SBState
+        sb_action = obs.action # SBAction
+        sb_intermediate_states = obs.intermediate_states
+        reward = obs.reward
+
+        return sb_state, sb_action, sb_intermediate_states, reward
+
+    def Unpack_SBState(self, sb_state):
+        objects = sb_state.objects
+        image = sb_state.image
+        game_state = sb_state.game_state
+        sling = sb_state.sling
+
+        return objects, image, game_state, sling
+
+    def Obs_to_StateImage(self, obs):
+        sb_state, sb_action, sb_intermediate_states, reward = self.Unpack_ScienceBirdsObservation(obs)
+        state, image , _ , _ = self.Unpack_SBState(sb_state)
+
+        return state, image
+
+
+class NoveltyDetector():
+    def __init__(self, model_path, class_info_path):
+        self.thresholds = np.load(class_info_path)['thresholds']
+
+        self.class_list = np.load(class_info_path)['class_list']
+        self.novelty_model = ModelCosine(len(self.class_list))
+        self.novelty_model.load_state_dict(torch.load(model_path))
+
+        self.weight_ll = self.novelty_model.classifier[4].weight.detach().cpu().numpy()
+        # self.image = image
+        # self.state = state
+    def init_state(self,state, image):
+        self.image = image
+        self.state = state
+
+    def evalaute(self):
+        """
+        returns list of subImgs
+        """
+        keys = list(self.state.keys())
+        subImgs = []
+        id = 0
+        for k in range(len(keys)):
+            object = self.state[keys[k]]
+            type = object['type']
+            poly = object['polygon']
+            if poly.type == "Polygon":
+                subImg = self.img_to_subImg(type, poly)
+                activations_ll = self.extract_activations(subImg)
+                novelty, type_preidcted = self.detect_novelty(activations_ll)
+                print(novelty, type, type_preidcted)
+                ## do novelty detection
+            elif poly.type == "MultiPolygon":
+                for p in range(len(poly)):
+                    subImg = self.img_to_subImg(type, poly[p])
+                    activations_ll = self.extract_activations(subImg)
+                    novelty, type_preidcted = self.detect_novelty(activations_ll)
+                    print(novelty, type, type_preidcted)
+                ## do novelty detection
+
+    def extract_activations(self,subImg):
+        trans = transforms.Compose([transforms.ToTensor()])
+        subImg_tensor = trans(subImg)
+        subImg_tensor_reshaped =  subImg_tensor.reshape((1,3,32,32))
+
+        # subImg_tensor = torch.tensor(subImg)
+        self.novelty_model.eval()
+        with torch.no_grad():
+            layers, _ = self.novelty_model(subImg_tensor_reshaped)
+        act = layers[len(layers)-2].cpu().numpy()
+        return act
+
+    def detect_novelty(self,act):
+
+        scores = np.matmul(act,self.weight_ll.T)[0]
+
+        max_score = np.max(scores)
+        max_score_class = np.argmax(scores)
+
+        if max_score > self.thresholds[max_score_class]:
+            novelty = False
+
+
+            return novelty, self.class_list[max_score_class]
+        novelty = True
+        return novelty, self.class_list[max_score_class]
+
+
+    def img_to_subImg(self,type, poly):
+
+        x, y = poly.exterior.coords.xy
+        poly_coords = np.int32(np.vstack((x,y)).T)
+
+        x_min = np.min(poly_coords[:,0])
+        x_max = np.max(poly_coords[:,0])
+        y_min = np.min(poly_coords[:,1])
+        y_max = np.max(poly_coords[:,1])
+        subImg = self.image[y_min:y_max,x_min:x_max,:]
+        subImg = cv2.resize(subImg,(32,32))
+        # subImg = Image.fromarray(image_type)
+
+
+        return subImg
---
+++ test_novelty_detector.py
@@ -0,0 +1,25 @@
+import os
+from agent.perception.novelty_detection.novelty_detector import ObsToState
+from agent.perception.novelty_detection.novelty_detector import NoveltyDetector
+
+import pickle
+import pdb
+
+
+
+if __name__=="__main__":
+    model_path = "agent/perception/novelty_detection/params/model.pt"
+    class_info_path = "agent/perception/novelty_detection/params/class_info.npz"
+    novelty_detector = NoveltyDetector(model_path, class_info_path)
+
+    data_path = 'angry_birds/data/20211012_level_1_type_10_novelties/level_1_type_10_novelties/baseline/observations/'
+    data_obs = os.listdir(data_path)
+    Obs_to_State = ObsToState()
+
+    for obs in data_obs:
+        print(obs)
+        observation = pickle.load(open(data_path+obs,"rb"))
+        state, image = Obs_to_State.Obs_to_StateImage(observation)
+        novelty_detector.init_state(state, image)
+        novelty_detector.evalaute()
+        
