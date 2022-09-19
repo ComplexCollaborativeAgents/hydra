802f897b5444118aaad4a84aa780f9b88419d55b
jale <jale@parc.com>
2022-09-19 12:40:03 -1000

    Update episode log

--- agent/planning/polycraft_planning/polycraft_episode_log.py
+++ agent/planning/polycraft_planning/polycraft_episode_log.py
@@ -1,34 +1,54 @@
 import logging
 from typing import List
 
+from worlds.polycraft_world import PolycraftAction, PolycraftState
 from agent.consistency.observation import HydraEpisodeLog
 from agent.planning.pddl_plus import PddlPlusPlan, PddlPlusState
 from agent.planning.polycraft_meta_model import PolycraftMetaModel
 
 
 class PolycraftEpisodeLog(HydraEpisodeLog):
-    """ An object that represents an observation of the SB game """
+    """An object that represents an observation of the SB game 
+    """
 
-    def __init__(self, meta_model):
-        self.states = []  # A sequence of polycraft states
-        self.actions = []  # A sequence of polycraft actions
-        self.rewards = []  # The reward obtained from performing each action
+    states: List[PolycraftState]    # A sequence of polycraft states
+    actions: List[PolycraftAction]  # A sequence of polycraft actions
+    rewards: List[float]            # The reward obtained from performing each action
+    meta_model: PolycraftMetaModel
+    time_so_far: float              # The time since an episode has started
+
+    def __init__(self, meta_model: PolycraftMetaModel):
+        self.states = []  
+        self.actions = [] 
+        self.rewards = []
         self.meta_model = meta_model
         self.time_so_far = 0.0
         self.pddl_states = []
 
-    def get_initial_state(self):
-        return self.states[0]  # self.meta_model.create_pddl_state(self.states[0])
+    def get_initial_state(self) -> PolycraftState:
+        """Get the initial state of the episode log
+
+        Returns:
+            PolycraftState: Polycraft World state object
+        """
+        return self.states[0]
 
     def get_pddl_states_in_trace(self,
                                  meta_model: PolycraftMetaModel) -> List[PddlPlusState]:
         # TODO: Refactor and move this to the meta model?
-        """ Returns a sequence of PDDL states that are the observed intermediate states """
+        """Returns a sequence of PDDL states that are the observed intermediate states
+
+        Args:
+            meta_model (PolycraftMetaModel): Meta Model that contains the intermediate states
+
+        Returns:
+            List[PddlPlusState]: List of pddl states
+        """
         if len(self.pddl_states) < len(self.states):
             self.pddl_states = [meta_model.create_pddl_state(state) for state in self.states]
         return self.pddl_states
 
-    def get_pddl_plan(self, meta_model: PolycraftMetaModel):
+    def get_pddl_plan(self, meta_model: PolycraftMetaModel) -> PddlPlusPlan:
         """ Returns a PDDL+ plan object with the actions we performed """
         return PddlPlusPlan(self.actions)
 
