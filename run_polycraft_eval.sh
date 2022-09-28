6cfd21b5d34937549d6320ef17739f2efd63fca6
yonsher <yonsher@parc.com>
2022-09-28 19:08:08 +0300

    sh to run on Yoni's computer

--- run_polycraft_eval.sh
+++ run_polycraft_eval.sh
@@ -1,2 +1,4 @@
+export PATH="/mnt/c/Users/yonsher/PycharmProjects/hydra:/home/yonsher/miniconda3/envs/hydra/bin:$PATH"
+export PYTHONPATH="/mnt/c/Users/yonsher/PycharmProjects/hydra:/home/yonsher/miniconda3/envs/hydra/bin:$PYTHONPATH"
 
 python3 ./runners/polycraft_eval_runner.py
